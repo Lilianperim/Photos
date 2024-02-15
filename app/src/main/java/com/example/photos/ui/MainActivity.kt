4d436a31eb627e839cd0af96c498444d243ef8c0
Lilianperim <liliancrisperim@gmail.com>
2024-02-15 16:17:15 -0300

    Add layout toolbar and search view

--- app/src/main/java/com/example/photos/ui/MainActivity.kt
+++ app/src/main/java/com/example/photos/ui/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.photos.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.photos.R
+import com.example.photos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(binding.root)
+        setupToolBar()
     }
+
+    private fun setupToolBar() {
+        setSupportActionBar(binding.toolBar.apply {
+            title = getString(R.string.app_name)
+        })
+    }
+
 }
