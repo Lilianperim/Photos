b3174ca398309bcc83c381906ea17d6930dae344
Lilianperim <liliancrisperim@gmail.com>
2024-02-16 15:17:59 -0300

    initial setup SearchView adapter and listeners

--- app/src/main/java/com/example/photos/ui/MainActivity.kt
+++ app/src/main/java/com/example/photos/ui/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.example.photos.ui
 
+import android.app.SearchManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.cursoradapter.widget.SimpleCursorAdapter
 import com.example.photos.R
 import com.example.photos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private val binding: ActivityMainBinding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
-    }
+    private lateinit var binding: ActivityMainBinding
+    private var cursorAdapter: SimpleCursorAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setupToolBar()
+        setupSearchView()
     }
 
     private fun setupToolBar() {
@@ -23,4 +27,29 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
+    private fun setupSearchView() = with(binding) {
+        val from = arrayOf(SearchManager.SUGGEST_COLUMN_TEXT_1)
+        val to = intArrayOf(android.R.id.text1)
+
+        cursorAdapter = SimpleCursorAdapter(
+            this@MainActivity,
+            android.R.layout.simple_list_item_1,
+            null,
+            from,
+            to,
+            SimpleCursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER
+        )
+
+        svPhoto.suggestionsAdapter = cursorAdapter
+
+        svPhoto.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
+    }
 }
